c34d7ba3075ae8e11eee3dad4d95cb944c087843
Momin Khan <foolmoron@gmail.com>
2020-02-01 17:06:06 -0500

    Login with name + random name test

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class GameManager : Manager<GameManager> {
+
+    public string Name;
+    public Random Rand;
+
+    public GameObject[] LoginObjects;
+    public Button DefaultButton;
+
+    static int hash_djb2(string s) {
+        uint hash = 5381;
+        foreach (var c in s) {
+            hash = (hash << 5) + hash + c;
+        }
+        return (int)hash;
+    }
+
+    public void Login(string n) {
+        Name = n;
+
+        // generate all the random stuff here using the inputted name as a random seed
+        using (var r = new WithRandomSeed(hash_djb2(Name))) {
+            for (int i = 0; i < 5; i++) {
+                Debug.Log("Random name = " + NameGenerator.Inst.GetRandomFirstName() + " " + NameGenerator.Inst.GetRandomLastName());
+            }
+        }
+
+        // setup computer
+        LoginObjects.ForEach(go => go.SetActive(false));
+        DefaultButton.OnPointerClick(new PointerEventData(EventSystem.current));
+        DefaultButton.Select();
+    }
+}
---
+++ Assets/Scripts/NameGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NameGenerator : Manager<NameGenerator> {
+
+    public TextAsset FirstNamePrefixes;
+    public TextAsset FirstNameSuffixes;
+    public TextAsset LastNamePrefixes;
+    public TextAsset LastNameSuffixes;
+
+    string[] firstNamePrefixes;
+    string[] firstNameSuffixes;
+    string[] lastNamePrefixes;
+    string[] lastNameSuffixes;
+
+    void Do()
+    {
+        firstNamePrefixes = FirstNamePrefixes.text.Split('\n').Map(s => s.Replace("\r", ""));
+        firstNameSuffixes = FirstNameSuffixes.text.Split('\n').Map(s => s.Replace("\r", ""));
+        lastNamePrefixes = LastNamePrefixes.text.Split('\n').Map(s => s.Replace("\r", ""));
+        lastNameSuffixes = LastNameSuffixes.text.Split('\n').Map(s => s.Replace("\r", ""));
+    }
+
+    public string GetRandomFirstName() {
+        if (firstNamePrefixes == null) Do();
+        var i1 = Mathf.FloorToInt(firstNamePrefixes.Length * Random.value);
+        var i2 = Random.value > 0.5f ? i1 : Mathf.FloorToInt(firstNameSuffixes.Length * Random.value);
+        return firstNamePrefixes[i1] + firstNameSuffixes[i2];
+    }
+    public string GetRandomLastName() {
+        if (firstNamePrefixes == null) Do();
+        var i1 = Mathf.FloorToInt(lastNamePrefixes.Length * Random.value);
+        var i2 = Random.value > 0.5f ? i1 : Mathf.FloorToInt(lastNameSuffixes.Length * Random.value);
+        return lastNamePrefixes[i1] + lastNameSuffixes[i2];
+    }
+}
