dbe8d3cb8aa0ca5c303eb224837a9687ffd5ea2a
Momin Khan <foolmoron@gmail.com>
2020-02-01 13:57:50 -0500

    Mouse look

---
+++ Assets/Scripts/MouseLooking.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseLooking : MonoBehaviour {
+
+    public AnimationCurve ScreenToPosX = AnimationCurve.Linear(0, 0, 1, 1);
+    public AnimationCurve ScreenToPosY = AnimationCurve.Linear(0, 0, 1, 1);
+    [Range(0f, 0.25f)]
+    public float LerpSpeed = 0.1f;
+
+    new Camera camera;
+
+    void Awake() {
+        camera = GetComponent<Camera>();
+    }
+
+    void Update() {
+        var mousePos = camera.ScreenToViewportPoint(Input.mousePosition);
+        var desiredPos = new Vector2(ScreenToPosX.Evaluate(mousePos.x), ScreenToPosY.Evaluate(mousePos.y));
+        transform.position = Vector2.Lerp(transform.position, desiredPos, LerpSpeed).to3(transform.position.z);
+    }
+}
