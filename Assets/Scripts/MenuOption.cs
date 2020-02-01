6b766ac7853b681157c80ad9b14309b2736e176f
Momin Khan <foolmoron@gmail.com>
2020-02-01 12:55:18 -0500

    Window toggling

---
+++ Assets/Scripts/MenuOption.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MenuOption : MonoBehaviour {
+    
+    public Panel ConnectedPanel;
+
+    Image background;
+    TextMeshProUGUI text;
+
+    void Awake() {
+        background = transform.GetComponent<Image>();
+        text = transform.Find("Text").GetComponent<TextMeshProUGUI>();
+
+        GetComponent<Button>().onClick.AddListener(() => ConnectedPanel.Show());
+    }
+
+    void Update() {
+        ConnectedPanel.MainColor = background.color;
+        ConnectedPanel.Title = text.text;
+    }
+}
---
+++ Assets/Scripts/Panel.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Panel : MonoBehaviour {
+
+    public static readonly List<Panel> AllInScene = new List<Panel>(5);
+
+    public Color MainColor = Color.white;
+    public string Title = "";
+
+    Image background;
+    TextMeshProUGUI title;
+
+    void Awake() {
+        AllInScene.Add(this);
+
+        background = transform.GetComponent<Image>();
+        title = transform.Find("Title").GetComponent<TextMeshProUGUI>();
+    }
+
+    void OnDestroy() {
+        AllInScene.Remove(this);
+    }
+
+    void Update() {
+        background.color = MainColor;
+        title.text = Title;
+    }
+
+    public void Show() {
+        foreach (var panel in AllInScene) {
+            panel.gameObject.SetActive(false);
+        }
+        gameObject.SetActive(true);
+    }
+}
